513580a8319173f7cc164e1b944377d4674cbbb4
Frank Grießhammer <fgriessh@adobe.com>
2015-04-01 19:33:23 -0700

    Add module-finding magic for FontLab, which is lacking the __file__ object.

--- TrueType/InputTrueTypeHints_coords.py
+++ TrueType/InputTrueTypeHints_coords.py
@@ -4,15 +4,38 @@
 import os
 import sys
 
-def findModulePath(name, path):
+def findModulePath(moduleName, path):
     for root, dirs, files in os.walk(path):
-        if name in files:
+        if moduleName in files:
             return os.path.join(root)
+    else:
+        return None
 
-modPath = findModulePath('InputTrueTypeHints.py', fl.usercommonpath)
+moduleName = 'InputTrueTypeHints.py'
+customModulePathMAC = os.path.join('~', 'Library', 'Application Support', 'FontLab', 'Studio 5', 'Macros')
+customModulePathPC = os.path.join('~', 'Documents', 'FontLab', 'Studio5', 'Macros')
 
-if not modPath in sys.path:
-    sys.path.append(modPath)
+customModulePathMAC = os.path.expanduser(customModulePathMAC)
+customModulePathPC = os.path.expanduser(customModulePathPC)
+possibleModulePaths = [fl.userpath, customModulePathMAC, customModulePathPC]
 
-import InputTrueTypeHints
-InputTrueTypeHints.preRun(coord_option=True)
+print '\nLooking for %s ... ' % (moduleName)
+for path in possibleModulePaths:
+    modPath = findModulePath(moduleName, path)
+    if modPath:
+        print 'found at %s' % modPath
+        break
+
+if not modPath:
+    # Module was not found. World ends.
+    print 'Not found in the following folders:\n%s\n\
+Please make sure the possibleModulePaths list in this script \
+points to a folder containing %s' % ('\n'.join(possibleModulePaths), moduleName)
+
+else:
+    # Module was found, import it and run it.
+    if not modPath in sys.path:
+        sys.path.append(modPath)
+
+    import InputTrueTypeHints
+    InputTrueTypeHints.preRun(coord_option=True)
