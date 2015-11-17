afbf45a614fdc662a7426b76d835194bff5258fd
Frank Grießhammer <fgriessh@adobe.com>
2015-11-16 20:36:33 -0800

    Do not write bogus data to tthints file. When a point index problem existed in a given glyph, the resulting hinting instruction line contained empty data, which made the hint input choke. No more!

--- TrueType/outputTTHints.py
+++ TrueType/outputTTHints.py
@@ -3,33 +3,33 @@
 
 __copyright__ = __license__ =  """
 Copyright (c) 2015 Adobe Systems Incorporated. All rights reserved.
- 
+
 Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the 
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
- 
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
- 
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
 __doc__ = """
 Output TrueType Hints
 
-This FontLab macro will write a simple text file `tthints` which contains 
-TrueType instructions and point indexes for each selected glyph. 
-If the external file already exists, the script will replace the existing 
-entries and add new entries as needed. 
+This FontLab macro will write a simple text file `tthints` which contains
+TrueType instructions and point indexes for each selected glyph.
+If the external file already exists, the script will replace the existing
+entries and add new entries as needed.
 
 The script will emit an error if there are hints attached to off-curve
 points, but will still write those hints.
@@ -47,7 +47,7 @@ v1.4 - Apr 17 2015 - Always write to a file named 'tthints' (even if the
 v1.3 - Mar 25 2015 - Allow optional coordinate output.
 					 Allow hinting sidebearings.
 v1.2 - Mar 23 2015 – Allow instructions in x-direction.
-v1.1 - Mar 04 2015 – Change name to Output TrueType Hints to supersede 
+v1.1 - Mar 04 2015 – Change name to Output TrueType Hints to supersede
 					 the old script of the same name.
 v1.0 - Nov 27 2013 - Initial release
 """
@@ -83,7 +83,7 @@ def readTTHintsFile(filePath):
 	lines = data.splitlines()
 
 	ttHintsList = []
-	
+
 	for i in range(len(lines)):
 		line = lines[i]
 		# Skip over blank lines
@@ -95,7 +95,7 @@ def readTTHintsFile(filePath):
 			continue
 		else:
 			ttHintsList.append(line)
-	
+
 	return ttHintsList
 
 
@@ -108,25 +108,25 @@ def writeTTHintsFile(content, filePath):
 def processTTHintsFileData(ttHintsList):
 	storedGlyphs = []
 	ttHintsDict = {}
-	
+
 	for item in ttHintsList:
 		hintItems = item.split("\t")
-		
+
 		if len(hintItems) not in [2,3]:
 			print "\tERROR: This hint definition does not have the correct format\n\t%s" % item
 			continue
-		
+
 		gName = hintItems[0]
 		ttHintsDict[gName] = item
 		storedGlyphs.append(gName)
-	
+
 	return storedGlyphs, ttHintsDict
 
 
 def analyzePoint(glyph, nodeIndex):
 	'''Analyzes a given point for a given glyph.
 	   In a normal case, this function returns a tuple of point index and point coordinates.
-	   If the sidebearings have been hinted, this function returns a tuple containing the flag 
+	   If the sidebearings have been hinted, this function returns a tuple containing the flag
 	   for the appropriate sidebearing.
 
 	'''
@@ -136,23 +136,26 @@ def analyzePoint(glyph, nodeIndex):
 
 	except:
 		''' This happens if left or right bottom point have been hinted.
-			In the hinting code, those points are stored as point indices greater than the actual
-			point count of the glyph. Since those points cannot be grasped in the context of the
-			outline, flags are created:
+			In the hinting code, those points are stored as point indices
+			greater than the actual point count of the glyph.
+			Since those points cannot be grasped in the context of the outline,
+			flags are created:
+
 			"BL" is bottom left
 			"BR" is bottom right.
 
-			Those flags are written into the output file as strings with quotes, so they can be parsed
-			with eval() when reading the file later.'''
+			Those flags are written into the output file as strings with quotes,
+			so they can be parsed with eval() when reading the file later.
+		'''
 
 		if nodeIndex == len(glyph):
 			point_flag = '"BL"'
 		elif nodeIndex == len(glyph) + 1:
 			point_flag = '"BR"'
 		else:
-			'point not in glyph -- which should not happen, but you never know.'
-			print '\tERROR: Hinting problem in glyph %s' % glyph.name
-			return '',''
+			'Point not in glyph -- which should not happen, but you never know.'
+			print '\tERROR: Point index problem in glyph %s' % glyph.name
+			return None, None
 
 		return point_flag, point_flag
 
@@ -170,7 +173,7 @@ def collectInstructions(tth, glyph, coord_option):
 		for writing the data into an external text file.
 
 
-		tth data structure: 
+		tth data structure:
 		-------------------
 
 		alignment points:
@@ -237,6 +240,9 @@ def collectInstructions(tth, glyph, coord_option):
 			coord_command = []
 			index_command = []
 
+		if None in coord_command:
+			' hinting error has been picked up by anyalyzePoint()'
+			return []
 
 		coord_command_string = ','.join(map(str, coord_command))
 		coord_command_string = coord_command_string.replace(' ', '')
@@ -256,23 +262,25 @@ def processGlyphs(selectedGlyphs, storedGlyphs, ttHintsDict, coord_option):
 	# This way the order of the items in the output file will be constant and predictable.
 
 	allGlyphsHintList = ["# Glyph name\tTT hints\n"]
-	
+
 	for glyph in fl.font.glyphs:
 		gName = glyph.name
-		
+
 		if (gName in selectedGlyphs) or (gName in storedGlyphs):
 			if gName in selectedGlyphs:
 				# If the glyph is selected, read its hints.
 				tth = TTH(glyph)
 				tth.LoadProgram()
-		
+
 				if len(tth.commands):
-					gHints = "%s\t%s\n" % (gName, ';'.join(collectInstructions(tth, glyph, coord_option)))
-					allGlyphsHintList.append(gHints)
+					instructionsList = collectInstructions(tth, glyph, coord_option)
+					if len(instructionsList):
+						gHints = "%s\t%s\n" % (gName, ';'.join(instructionsList))
+						allGlyphsHintList.append(gHints)
 				else:
 					print "WARNING: The glyph %s has no TrueType hints." % gName
 			else:
-				# If the glyph is not selected but present in the external 
+				# If the glyph is not selected but present in the external
 				# file, just read the data from the ttHintsDict and dump it.
 				allGlyphsHintList.append(ttHintsDict[gName] + '\n')
 
@@ -288,7 +296,7 @@ def run(font, parentDir, coord_option):
 	if not len(selectedGlyphs):
 		print "Select the glyph(s) to process and try again."
 		return
-	
+
 	if os.path.exists(tthintsFilePath):
 		print "WARNING: The %s file at %s will be modified." % (kTTHintsFileName, tthintsFilePath)
 		ttHintsList = readTTHintsFile(tthintsFilePath)
@@ -296,16 +304,16 @@ def run(font, parentDir, coord_option):
 	else:
 		ttHintsList = []
 		modFile = False
-	
+
 	if len(ttHintsList):
 		storedGlyphs = processTTHintsFileData(ttHintsList)[0]
 		ttHintsDict = processTTHintsFileData(ttHintsList)[1]
 	else:
 		storedGlyphs = []
 		ttHintsDict = {}
-	
+
 	allGlyphsHintList = processGlyphs(selectedGlyphs, storedGlyphs, ttHintsDict, coord_option)
-	
+
 	if len(allGlyphsHintList) > 1:
 		writeTTHintsFile(allGlyphsHintList, tthintsFilePath)
 	else:
@@ -314,20 +322,20 @@ def run(font, parentDir, coord_option):
 
 	if not modFile:
 		print "File %s was saved at %s" % (kTTHintsFileName, tthintsFilePath)
-	
+
 	print "Done!"
 
 
 def preRun(coord_option=False):
 	# Clear the Output window
 	fl.output = '\n'
-	
+
 	if fl.count == 0:
 		print "Open a font first."
 		return
 
 	font = fl.font
-	
+
 	if len(font) == 0:
 		print "The font has no glyphs."
 		return
@@ -337,7 +345,7 @@ def preRun(coord_option=False):
 	except AttributeError:
 		print "The font has not been saved. Please save the font and try again."
 		return
-	
+
 	run(font, parentDir, coord_option)
 
 
